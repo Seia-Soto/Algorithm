79bb76f2fe74367d174c192ede37098040d9d9cf
Seia-Soto <seia@outlook.kr>
2020-06-04 00:46:12 +0900

    Update JavaScript source for acmicpc-1001

---
+++ sources/acmicpc/1001/1001.js
@@ -0,0 +1,9 @@
+var fs = require('fs')
+
+var input = fs.readFileSync('/dev/stdin').toString()
+var params = input.split(' ')
+
+var a = parseInt(params[0])
+var b = parseInt(params[1])
+
+console.log(a - b)
